226c947f4d4bafcabb66184aae13b6af7e716e76
xiaomocqk <xiaomocqk@163.com>
2017-03-09 19:20:47 +0800

    vue-movie

---
+++ Vue/VUE-MOVIE/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import vHeader from './components/header/header.vue'
+// import Recommand from './components/recommand.vue'
+// import {swiper, swiperSlide} from 'vue-awesome-swiper'
+ 
+export default {
+    components:{
+      vHeader
+    }
+
+}
+</script>
+
+<style>
+html,body{
+  height: 100%;
+  overflow: hidden;
+}
+body{
+  font-size: 62.5%;
+  max-width: 500px;
+  min-width: 414px;
+  margin:0 auto;
+}
+#app {
+    height: 100%;
+    overflow: auto;
+    font-family: 'FontAwesome', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: #E5E9F2;
+    color: #2c3e50;
+}
+</style>
---
+++ Vue/VUE-MOVIE/src/components/header/header.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="header">
+		<div><img class="logo" src="../../assets/logo.png" alt="logo"></div>
+		<div>
+			<h3>喵眼电影</h3>
+			<span class="slogan">查影讯，上喵眼电影就够了！</span>
+		</div>
+		<input v-model="key" type="text" name="key" id="key" placeholder="请输入电影名称">
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				key:''
+			}
+		}
+	}
+</script>
+
+<style rel="stylesheet/less" lang="less">
+	.header{
+		display: flex;
+		height: 60px;
+	    padding: 10px;
+	    box-sizing: border-box;
+		align-items:center;
+
+		.logo{
+			width: 42px;
+			height: 42px;
+			margin-right: 10px;
+		}
+		h3{
+			font-weight: 700;
+			font-size: 14px;
+			margin-bottom: 6px;
+		}
+		.slogan{
+			font-size: 12px;
+			color: #999;
+		}
+		input{
+			width: 150px;
+			height: 24px;
+			border-radius: 6px;
+			border: 0;
+			text-indent: 10px;
+			outline: 0;
+			background: #fff;
+		}
+		>div:nth-child(2){
+			flex:1
+		}
+	}
+</style>
